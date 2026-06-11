0bc9ff10d81c96225b0da949d8c543bfd65e92b6
StephanieVA <stephanie1104@outlook.com>
2026-06-10 22:22:27 -0500

    Logs de diagnostico 2

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ console.log({
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
-
+console.log("Intentando conectar a MySQL...");
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
